dbc32cc7a56ef94a814ed94e40d81adf1380a033
brandcare-devs <webdev@brandcaredigital.com>
2026-07-30 16:32:16 +0400

    Update layout.tsx

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import type { Metadata } from 'next'
 
 import './globals.css'
+
+export const metadata: Metadata = {
+  verification: {
+    google: 'WvRxF-Ry4whDg0KQ5uQHWbfEH3IzliEcG7ansXESJo4',
+  },
+}
 
 export default function FrontendLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
