1cdb283a6e1346fadb325507e5a01bb2d5ed9001
brandcare-devs <webdev@brandcaredigital.com>
2026-07-30 17:26:13 +0400

    Adding GA Verification Snippet

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 import './globals.css'
 
@@ -9,7 +10,22 @@ export const metadata: Metadata = {
   },
 }
 
-export default function FrontendLayout({ children }: { children: React.ReactNode }) 
-{
-  return <>{children}</>
+export default function FrontendLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XNYD4HPL3M"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XNYD4HPL3M');
+        `}
+      </Script>
+      {children}
+    </>
+  )
 }
